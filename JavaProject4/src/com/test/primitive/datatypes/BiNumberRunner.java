fe2f481c9a80717838b0fad00020099d4549d745
Youngsik Yoon <dudtlr6512@naver.com>
2025-06-06 17:25:58 +0900

    0606

---
+++ JavaProject4/src/com/test/primitive/datatypes/BiNumberRunner.java
@@ -0,0 +1,19 @@
+package com.test.primitive.datatypes;
+
+public class BiNumberRunner {
+
+	public static void main(String[] args) {
+		
+		BiNumber numbers = new BiNumber(2, 3);
+
+		System.out.println(numbers.add());
+		System.out.println(numbers.multiply());
+
+		numbers.doubleValue();
+
+		System.out.println(numbers.getNumber1());
+		System.out.println(numbers.getNumber2());
+
+	}
+
+}
---
+++ JavaProject4/src/com/test/primitive/datatypes/SimpleInterestCalculator.java
@@ -0,0 +1,21 @@
+package com.test.primitive.datatypes;
+
+import java.math.BigDecimal;
+
+public class SimpleInterestCalculator {
+
+	BigDecimal principal;
+	BigDecimal interest;
+
+	public SimpleInterestCalculator(String principal, String interest) {
+		this.principal = new BigDecimal(principal);
+		this.interest = new BigDecimal(interest).divide(new BigDecimal(100));
+	}
+
+	public BigDecimal calculateTotalValue(int noOfYears) {
+		// Total Value = principal + principal * interest * noOfYear
+		BigDecimal noOfYearsBigDecimal = new BigDecimal(noOfYears);
+		BigDecimal totalValue = principal.add(principal.multiply(interest).multiply(noOfYearsBigDecimal));
+		return totalValue;
+	}
+}
